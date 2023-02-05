90ee20eafe521e4fbc76c62b9723cf06bcc19581
Prabhat Patel <prabhatpatel508@outlook.com>
2023-02-05 12:27:39 +0530

    news fatch successfully

--- js/script.js
+++ js/script.js
@@ -5,12 +5,29 @@ let query = window.location.search.split("?")[1].split("&")[0].split("=")[1];
 let page = parseInt(window.location.search.split("?")[1].split("&")[1].split("=")[1]);
 console.log(query , page)
 const fetchNews= async (query,page)=>{
-    let a = await fetch('/api?q=${query}&apiKey=dbe57b028aeb41e285a226a94865f7a7')
-    let r=await a.json()
+    // let a = await fetch('/api?q=${query}&apiKey=d99e1b37ae004e808b0ee06e896192eb')
+    let a = await fetch('api?q=${query}&apiKey=d99e1b37ae004e808b0ee06e896192eb')
+    // https://newsapi.org/v2/everything?q=bitcoin&apiKey=d99e1b37ae004e808b0ee06e896192eb
+    let r = await a.json()
     console.log(r)
-    totalPageesc = Math.ceil(r.totalResults/articlesPerPage)
+    totalPages = Math.ceil(r.totalResults/articlesPerPage)
     pre.href = `/?q=${query}&pageno=${page-1}`
-    pre.href = `/?q=${query}&pageno=${page+1}`
+    next.href = `/?q=${query}&pageno=${page+1}`
+    let str = ""
+    for(let item of r.articles){
+        str = str + `
+        <div class="col-sm-3">
+                    <div class="card text-bg-dark m-2" style="width: 18rem;">
+                        <img src="${item.urlToImage}" class="card-img-top" alt="...">
+                        <div class="card-body text-bg-dark">
+                            <h5 class="card-title">${item.title}</h5>
+                            <p class="card-text ">${item.description}</p>
+                            <a href="${item.url}" target="_blank" class="btn btn-primary">Read More...</a>
+                        </div>
+                    </div>
+                </div>`
+    }
+    content.innerHTML = str;
 }
 
-fatchNews("sports",page)
+fetchNews("sports",page) 
