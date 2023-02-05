cfe225b2cdebab488e671c4f3caae7918c3b08f2
Prabhat Patel <prabhatpatel508@outlook.com>
2023-02-05 15:21:24 +0530

    finally complete

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
 let articlesPerPage;
 let totalPages;
-console.log("Hello this is prabhat");
+console.log("Hey I am javascript")
 let query = window.location.search.split("?")[1].split("&")[0].split("=")[1];
 let page = parseInt(window.location.search.split("?")[1].split("&")[1].split("=")[1]);
-console.log(query , page)
-const fetchNews= async (query,page)=>{
-    // let a = await fetch('/api?q=${query}&apiKey=d99e1b37ae004e808b0ee06e896192eb')
-    let a = await fetch('api?q=${query}&apiKey=d99e1b37ae004e808b0ee06e896192eb')
-    // https://newsapi.org/v2/everything?q=bitcoin&apiKey=d99e1b37ae004e808b0ee06e896192eb
+console.log(query, page)
+const fetchNews = async (query, pageNo) =>
+{
+    let a = await fetch(`/api?q=${query}&apiKey=d99e1b37ae004e808b0ee06e896192eb&pageSize=8&page=${pageNo}`)
     let r = await a.json()
     console.log(r)
-    totalPages = Math.ceil(r.totalResults/articlesPerPage)
-    pre.href = `/?q=${query}&pageno=${page-1}`
-    next.href = `/?q=${query}&pageno=${page+1}`
+    queryText.innerHTML = query.replace("+", " ")
+    queryResults.innerHTML = r.totalResults
+    totalPages = Math.ceil(r.totalResults / articlesPerPage)
+    pre.href = `/?q=${query}&pageno=${page - 1}`
+    next.href = `/?q=${query}&pageno=${page + 1}`
     let str = ""
-    for(let item of r.articles){
+    for (let item of r.articles)
+    {
+        let date = new Date(item.publishedAt).toLocaleDateString()
         str = str + `
         <div class="col-sm-3">
-                    <div class="card text-bg-dark m-2" style="width: 18rem;">
-                        <img src="${item.urlToImage}" class="card-img-top" alt="...">
-                        <div class="card-body text-bg-dark">
-                            <h5 class="card-title">${item.title}</h5>
-                            <p class="card-text ">${item.description}</p>
-                            <a href="${item.url}" target="_blank" class="btn btn-primary">Read More...</a>
-                        </div>
-                    </div>
-                </div>`
+              <div class="card text-bg-dark m-2" style="width: 18rem;">
+                <img src="${item.urlToImage}" class="card-img-top" alt="...">
+                <div class="card-body text-bg-dark">
+                    <h5 class="card-title">${item.title}</h5>
+                    <span class="fw-bold">Published </span> : ${date}
+                    <p class="card-text">${item.description}</p>
+                    <a target="_blank" href="${item.url}" class="btn btn-success">Read More...</a>
+                </div>
+            </div>
+        </div>    
+        `
     }
     content.innerHTML = str;
 }
 
-fetchNews("sports",page) 
+fetchNews(query, page)
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html',{root: path.join(__dirname)});
+  res.sendFile('index.html',{root: path.join(__dirname)})
 })
 
 app.get('/api', async(req, res) => {
-  console.log(req._parsedUrl.query);
+  console.log(req._parsedUrl.query)
   let url = "https://newsapi.org/v2/everything?"+req._parsedUrl.query
   let r = await axios(url)
-  let a = r.data
+  let a = r.data 
   res.json(a)
 })
 
